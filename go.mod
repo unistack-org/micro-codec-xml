22593a9effcf7fae23533b83a776dee3e57fa683
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-12-01 00:07:38 +0300

    initial import of xml codec
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/unistack-org/micro-codec-xml
+
+go 1.15
+
+require github.com/unistack-org/micro/v3 v3.0.2-0.20201123132107-c9049c384502
---
+++ xml.go
@@ -0,0 +1,78 @@
+// Package xml provides a xml codec
+package xml
+
+import (
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+
+	"github.com/unistack-org/micro/v3/codec"
+)
+
+type xmlCodec struct{}
+
+func (c *xmlCodec) Marshal(b interface{}) ([]byte, error) {
+	switch m := b.(type) {
+	case nil:
+		return nil, nil
+	case *codec.Frame:
+		return m.Data, nil
+	}
+
+	return xml.Marshal(b)
+}
+
+func (c *xmlCodec) Unmarshal(b []byte, v interface{}) error {
+	if b == nil {
+		return nil
+	}
+	switch m := v.(type) {
+	case nil:
+		return nil
+	case *codec.Frame:
+		m.Data = b
+		return nil
+	}
+
+	return xml.Unmarshal(b, v)
+}
+
+func (c *xmlCodec) ReadHeader(conn io.ReadWriter, m *codec.Message, t codec.MessageType) error {
+	return nil
+}
+
+func (c *xmlCodec) ReadBody(conn io.ReadWriter, b interface{}) error {
+	switch m := b.(type) {
+	case nil:
+		return nil
+	case *codec.Frame:
+		buf, err := ioutil.ReadAll(conn)
+		if err != nil {
+			return err
+		}
+		m.Data = buf
+		return nil
+	}
+
+	return xml.NewDecoder(conn).Decode(b)
+}
+
+func (c *xmlCodec) Write(conn io.ReadWriter, m *codec.Message, b interface{}) error {
+	switch m := b.(type) {
+	case nil:
+		return nil
+	case *codec.Frame:
+		_, err := conn.Write(m.Data)
+		return err
+	}
+
+	return xml.NewEncoder(conn).Encode(b)
+}
+
+func (c *xmlCodec) String() string {
+	return "xml"
+}
+
+func NewCodec() codec.Codec {
+	return &xmlCodec{}
+}
