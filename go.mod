31ea6d69892b9500713feb0ddf3d85bc8acccb24
Vasiliy Tolstov <v.tolstov@unistack.org>
2024-09-17 12:18:41 +0300

    update to latest micro, optimize flatten
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go.unistack.org/micro-codec-xml/v3
 
 go 1.16
 
-require go.unistack.org/micro/v3 v3.10.14
+require (
+	github.com/imdario/mergo v0.3.13 // indirect
+	go.unistack.org/micro-proto/v3 v3.4.1
+	go.unistack.org/micro/v3 v3.10.88
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- xml.go
+++ xml.go
@@ -3,7 +3,6 @@ package xml // import "go.unistack.org/micro-codec-xml/v3"
 
 import (
 	"encoding/xml"
-	"io"
 
 	pb "go.unistack.org/micro-proto/v3/codec"
 	"go.unistack.org/micro/v3/codec"
@@ -29,8 +28,11 @@ func (c *xmlCodec) Marshal(v interface{}, opts ...codec.Option) ([]byte, error) 
 	for _, o := range opts {
 		o(&options)
 	}
-	if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
-		v = nv
+
+	if options.Flatten {
+		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
+			v = nv
+		}
 	}
 
 	switch m := v.(type) {
@@ -53,8 +55,10 @@ func (c *xmlCodec) Unmarshal(b []byte, v interface{}, opts ...codec.Option) erro
 		o(&options)
 	}
 
-	if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
-		v = nv
+	if options.Flatten {
+		if nv, nerr := rutil.StructFieldByTag(v, options.TagName, flattenTag); nerr == nil {
+			v = nv
+		}
 	}
 
 	switch m := v.(type) {
@@ -67,41 +71,6 @@ func (c *xmlCodec) Unmarshal(b []byte, v interface{}, opts ...codec.Option) erro
 	}
 
 	return xml.Unmarshal(b, v)
-}
-
-func (c *xmlCodec) ReadHeader(conn io.Reader, m *codec.Message, t codec.MessageType) error {
-	return nil
-}
-
-func (c *xmlCodec) ReadBody(conn io.Reader, v interface{}) error {
-	if v == nil {
-		return nil
-	}
-
-	buf, err := io.ReadAll(conn)
-	if err != nil {
-		return err
-	} else if len(buf) == 0 {
-		return nil
-	}
-
-	return c.Unmarshal(buf, v)
-}
-
-func (c *xmlCodec) Write(conn io.Writer, m *codec.Message, v interface{}) error {
-	if v == nil {
-		return nil
-	}
-
-	buf, err := c.Marshal(v)
-	if err != nil {
-		return err
-	} else if len(buf) == 0 {
-		return codec.ErrInvalidMessage
-	}
-
-	_, err = conn.Write(buf)
-	return err
 }
 
 func (c *xmlCodec) String() string {
